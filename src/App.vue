9b76c56a667dffad1ad5ad1c1377df7e97a549f9
GalvinGao <galvingaoper@gmail.com>
2020-04-09 19:01:01 -0400

    feat(background): add background

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app>
-    <v-content>
-      <v-container>
+    <v-content class="transparent">
+      <Background />
+      <v-container class="transparent">
         <v-row align="start" justify="center">
           <div class="locale-switcher">
             <LocaleSwitcher />
@@ -17,10 +18,10 @@
               class="my-4"
             />
 
-            <h2 class="overline font-weight-bold text-center">
+            <h2 class="overline font-weight-bold text-center z-index-fix">
               {{ $t('app.name') }}
             </h2>
-            <h1 class="heading text-center">
+            <h1 class="heading text-center z-index-fix">
               {{ $t('app.title') }}
             </h1>
 
@@ -84,10 +85,11 @@
 import strings from "./utils/strings";
 import I18n from "./mixins/I18n";
 import LocaleSwitcher from "./components/LocaleSwitcher";
+import Background from "./components/Background";
 
 export default {
   name: 'App',
-  components: {LocaleSwitcher},
+  components: {Background, LocaleSwitcher},
   mixins: [I18n],
 
   data: () => ({
@@ -150,5 +152,21 @@ export default {
     right: 1em !important;
     top: 1em !important;
     z-index: 99999999;
+  }
+
+  .z-index-fix {
+    z-index: 5 !important;
+  }
+
+  .v-application, .v-application--wrap {
+    background: transparent !important;
+  }
+
+  .theme--dark.v-sheet {
+    background: rgba(25, 25, 25, .7) !important;
+  }
+
+  .theme--dark.v-sheet.v-alert {
+    background: rgba(0, 0, 0, .7) !important;
   }
 </style>
---
+++ src/components/Background.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-img
+    src="https://penguin.upyun.galvincdn.com/backgrounds/choose-mirror.jpg"
+    height="100vh"
+    width="100vw"
+    class="background"
+    gradient="to top, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.2) 25%, rgba(0, 0, 0, 0) 100%"
+
+    transition="fade-transition"
+  />
+
+</template>
+
+<script>
+  export default {
+    name: "Background"
+  }
+</script>
+
+<style scoped>
+.background {
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
+  filter: blur(3px);
+}
+</style>
